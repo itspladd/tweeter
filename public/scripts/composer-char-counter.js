f939c988a51b89d5e11f16c321b489138324393d
Paul Ladd <vlendle@gmail.com>
2021-03-23 18:50:48 +0000

    Move counter manipulation into helper functions. Add yellow-text class.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,26 @@
 $(document).ready( () => {
   const $counter = $('#tweetCharCounter');
-  const maxTweetChars = 140;
   $('textarea#tweet-text').on('input', function() {
-    length = this.value.length;
-    remaining = maxTweetChars - length;
-    $counter.text(remaining);
-    if (remaining < 0) {
-      $counter.css('color', 'red');
-    }
+    updateCounter($counter, this);
   });
 });
+
+const updateCounter = function($counter, textArea) {
+  const maxTweetChars = 140;
+  length = textArea.value.length;
+  remaining = maxTweetChars - length;
+  $counter.text(remaining);
+  toggleCounterColor($counter, remaining);
+};
+
+const toggleCounterColor = ($counter, remaining) => {
+  if (remaining > 20) {
+    $counter.attr('class', '');
+  }
+  if (remaining < 20 && remaining > 0) {
+    $counter.attr('class', 'yellow-text');
+  }
+  if (remaining <= 0) {
+    $counter.attr('class', 'red-text');
+  }
+}
