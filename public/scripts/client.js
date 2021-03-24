4f9377fbf812405980731e21817cbaa17f5a1942
Paul Ladd <vlendle@gmail.com>
2021-03-24 15:16:49 +0000

    Add proper variable declaration for jQuery elements in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,16 +30,48 @@ const data = [
   }
 ]
 
-const renderTweets = tweets => {
-  $container = $('#all-tweets');
-  for (let $tweet of tweets) {
-    $tweet = createTweetElement($tweet);
+const renderTweets = tweetDataArray => {
+  const $container = $('#all-tweets');
+  for (let $tweet of tweetDataArray) {
+    const $tweet = createTweetElement($tweet);
     $container.append($tweet);
   }
 };
 
 const createTweetElement = tweetData => {
-  $tweet = $(`
+  // Container element
+  const $tweet = $('<article>', { class: 'tweet' });
+  
+  // Header elements
+  const $header = $('<header>');
+  const $avatar = $('<img>', { src: tweetData.user.avatars });
+  const $username = $('<span>', { class: 'name' }).text(tweetData.user.name);
+  const $handle = $('<span>', { class: 'handle' }).text(tweetData.user.handle);
+  
+  // Actual tweet content
+  const $content = $('<section>').text(tweetData.content.text);
+
+  // Footer elements
+  const $footer = $('<footer>');
+  const $postDate = $('<time>', { class: 'postDate' }).text(tweetData.created_at);
+  
+  // Action buttons (inside footer)
+  // TODO: Change these to images, add interactivity via classes(?)
+  const $actions = $('<div>', { class: 'actions' });
+  const $flag = $('<span>').text('1');
+  const $retweet = $('<span>').text('2');
+  const $like = $('<span>').text('3');
+
+  // TWEET, ASSEMBLE!
+  $actions.append($flag, $retweet, $like);
+
+  $header.append($avatar, $username, $handle);
+  $footer.append($postDate, $actions);
+
+  $tweet.append($header, $content, $footer);
+
+//Alternate implementation with HTML structure in the JS.
+/*   $tweet = $(`
     <article class="tweet">
       <header>
         <img src="${tweetData.user.avatars}">
@@ -56,7 +88,7 @@ const createTweetElement = tweetData => {
           <span>3</span>
       </footer>
     </article>
-  `);
+  `); */
 
   return $tweet;
 };
